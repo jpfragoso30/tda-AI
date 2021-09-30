b0c34d13bcbd048574eac6ab57a3ecba5d50f991
anaremorfin <anaremorfin@gmail.com>
2021-09-30 16:58:36 -0500

    Create menuopciones.c

---
+++ src/menuopciones.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void)
+{
+  //Declaraciones
+
+  //Desplegar Menú
+    Menu();
+
+    scanf ("%d", &opcion);
+
+    //CASE
+}
+
+void Menu (void)
+{
+  printf("Seleccione una opcion:\n");
+  printf("1. Imprimir en orden\n 2. Buscar palabra\n 3.Salir\n");
+}
