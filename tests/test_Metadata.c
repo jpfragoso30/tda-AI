673732ee9b4560fc853f0657357c5682422a6e20
jpfragoso30 <werevertumorro44@live.com.mx>
2021-10-07 22:56:24 -0500

    [ADDED] setup for guardar metadatos unit testing

--- tests/test_Metadata.c
+++ tests/test_Metadata.c
@@ -5,6 +5,7 @@
 #include "../libs/unity.h"
 
 // #include(s) for unit testing files for each .c
+#include "./test_guardarMetadatos.c"
 
 void setUp(void)
 {
@@ -19,6 +20,7 @@ int main(void)
     UNITY_BEGIN();
 
     // RUN_TEST(functions..., __LINE__);
+    RUN_TEST(test_abrirArchivo, __LINE__);
 
     return UNITY_END();
 }
